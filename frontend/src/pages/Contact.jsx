235d5d9f26b22e416b62a25691fc54eeb9e1d118
Alok soni <boyalok149@gmail.com>
2024-12-24 14:09:05 +0530

    update backend

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -19,7 +19,7 @@ const Contact = () => {
     console.log("Form Data Submitted:", formData); // Debugging
 
     try {
-      const response = await fetch('http://localhost:5000/api/contact', {
+      const response = await fetch('https://recipe-finder-app-1clf.onrender.com/api/contact', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
